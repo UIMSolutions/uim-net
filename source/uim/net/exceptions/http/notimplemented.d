6ff27308a3852d56fd1fc457cd14bebf570914c0
UIMSolutions <ui.manufaktur@gmail.com>
2023-05-03 12:07:59 +0200

    Updates

---
+++ source/uim/net/exceptions/http/notimplemented.d
@@ -0,0 +1,16 @@
+/*********************************************************************************************************
+	Copyright: © 2015-2023 Ozan Nurettin Süel (Sicherheitsschmiede)                                        
+	License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file.  
+	Authors: Ozan Nurettin Süel (Sicherheitsschmiede)                                                      
+**********************************************************************************************************/module uim.net.https\Exception;
+
+/**
+ * Not Implemented Exception - used when an API method is not implemented
+ */
+class NotImplementedException : HttpException {
+
+    protected _messageTemplate = "%s is not implemented.";
+
+
+    protected _defaultCode = 501;
+}
